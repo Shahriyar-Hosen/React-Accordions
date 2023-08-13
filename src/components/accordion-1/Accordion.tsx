5fe9b2ea54108ccd67bd7cdeed5ee22faf8cff50
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-13 12:19:50 +0600

    data add

--- src/components/accordion-1/Accordion.tsx
+++ src/components/accordion-1/Accordion.tsx
@@ -1,29 +1,7 @@
 import { useState } from "react";
+import { faqs } from "../../data";
 import AccordionItem from "./AccordionItem";
 import "./styles.css";
-
-const faqs = [
-  {
-    id: 1,
-    header: "What is Lorem Ipsum?",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-  },
-  {
-    id: 2,
-    header: "Where does it come from?",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-  },
-  {
-    id: 3,
-    header: "Why do we use it?",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-  },
-  {
-    id: 4,
-    header: "Where can I get some?",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-  },
-];
 
 export const Accordion = () => {
   const [active, setActive] = useState<number | null>();
---
+++ src/data/index.ts
@@ -0,0 +1,24 @@
+import { IFaq } from "../types/accordionItem";
+
+export const faqs: IFaq[] = [
+  {
+    id: 1,
+    header: "What is Lorem Ipsum?",
+    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+  },
+  {
+    id: 2,
+    header: "Where does it come from?",
+    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+  },
+  {
+    id: 3,
+    header: "Why do we use it?",
+    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+  },
+  {
+    id: 4,
+    header: "Where can I get some?",
+    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+  },
+];
